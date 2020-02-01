bdc1ccba6e43ae9190c34b6359bb6fab6dc6facd
Geraldo Intmain <geraldolsribeiroim@gmail.com>
2020-01-31 22:06:41 -0300

    Redefine GR_JUMP_TRACE para {}

--- include/libnavajo/HttpRequest.hh
+++ include/libnavajo/HttpRequest.hh
@@ -15,7 +15,7 @@
 #define HTTPREQUEST_HH_
 
 // #define GR_JUMP_TRACE std::cerr << "\nGRJMP:" << __FILE__ << "/" << __LINE__ << "/" << __PRETTY_FUNCTION__ << std::endl;
-#define GR_JUMP_TRACE;
+#define GR_JUMP_TRACE {}
 
 #include <iostream>
 
