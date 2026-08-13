2b7b8b47663f02a852840af54de7e099d0f12fb7
Geraldo Ribeiro <geraldolsribeiro@gmail.com>
2026-08-13 19:34:31 +0400

    Port to macos Apple Silicon

--- include/libnavajo/MemcachedRepository.hh
+++ include/libnavajo/MemcachedRepository.hh
@@ -1,12 +1,14 @@
 #ifndef __MEMCACHED_REPOSITORY_HH__
 #define __MEMCACHED_REPOSITORY_HH__
 
-#include <libmemcached/memcached.hpp>
+#include <cinttypes>
 #include <memory>
 #include <string>
 #include <vector>
 
 using namespace std;
+
+#include <libmemcached/memcached.hpp>
 
 #include "WebRepository.hh"
 #include "libnavajo/GrDebug.hpp"
@@ -17,18 +19,21 @@ using namespace std;
 class MemcachedRepository : public WebRepository {
 private:
   shared_ptr<memcache::Memcache> mMemCacheClient;
-  string                         mPrefix;
-  string                         mServer;
-  int                            mPort;
-  time_t                         expiryTime(const time_t t);
-  bool                           get(const string &url, vector<char> &vec);
-  bool                           get(const string &url, string &value);
+  string mPrefix;
+  string mServer;
+  int mPort;
+  time_t expiryTime(const time_t t);
+  bool get(const string &url, vector<char> &vec);
+  bool get(const string &url, string &value);
 
 public:
-  MemcachedRepository(const string &prefix, const string &server = "127.0.0.1", const int port = 11211);
+  MemcachedRepository(const string &prefix, const string &server = "127.0.0.1",
+                      const int port = 11211);
   virtual ~MemcachedRepository() {};
-  bool set(const string &url, const vector<char> &vec, time_t expiry = 0, uint32_t flags = 0);
-  bool set(const string &url, const string &value, time_t expiry = 0, uint32_t flags = 0);
+  bool set(const string &url, const vector<char> &vec, time_t expiry = 0,
+           uint32_t flags = 0);
+  bool set(const string &url, const string &value, time_t expiry = 0,
+           uint32_t flags = 0);
   bool remove(const string &url);
   bool getFile(HttpRequest *request, HttpResponse *response) override;
   void freeFile(unsigned char *webpage) override;
